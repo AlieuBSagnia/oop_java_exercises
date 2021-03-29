ca642bec7694e6dc135a66a4c7bbec695ec3d980
Alieu <alieubsagnia@gmail.com>
2021-03-29 23:33:08 +0100

    LionCat now implements super function

--- src/main/java/com/techreturners/cats/LionCat.java
+++ src/main/java/com/techreturners/cats/LionCat.java
@@ -1,14 +1,8 @@
 package com.techreturners.cats;
 
 public class LionCat extends CatImpl {
-    private int height = 1100;
-    private String roar = "Roar!!!!";
 
-    public int getAverageHeight() {
-        return height;
-    }
-
-    public String eat() {
-        return roar;
+    public LionCat(){
+        super("Roar!!!!",1100,"wild");
     }
 }
